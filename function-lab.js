e9c5dbb5e1d3966196d3ab6b10728518a083d217
FaisalAlharbidev <164782595+FaisalAlharbidev@users.noreply.github.com>
2024-09-09 14:43:57 +0300

    Add files via upload
    function lab

---
+++ function-lab.js
@@ -0,0 +1,23 @@
+function numMultiply(num1,num2){
+    return console.log(num1*num2)
+}
+
+function numSubtract(num1,num2){
+    return console.log( num1-num2)
+}
+
+function numAdd(num1,num2){
+    return console.log( num1+num2 )
+}
+function numDivide(num1,num2){
+    return console.log(num1/num2)
+}
+
+
+numMultiply(2,2)
+
+numSubtract(3,4)
+
+numDivide(5,8)
+
+numAdd(10,4)
